d2bcdf1f9ac18bc8caa8fed4b551a46f5fcd8905
Your Name <you@example.com>
2022-02-20 22:42:20 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import { Input } from './Input';
 
 function App() {
   const h1 = <h1>React Form</h1>
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {h1} 
+        <Input />
       </header>
     </div>
   );
